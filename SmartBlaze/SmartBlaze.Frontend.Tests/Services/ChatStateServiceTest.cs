f81a3ffbdd5ac9eec904da304e1fd65d4c9af172
simone.giamboni <simone.giamboni@student.supsi.ch>
2024-06-20 11:27:28 +0200

    set up tests for frontend

---
+++ SmartBlaze/SmartBlaze.Frontend.Tests/Services/ChatStateServiceTest.cs
@@ -0,0 +1,22 @@
+using JetBrains.Annotations;
+using SmartBlaze.Frontend.Services;
+
+namespace SmartBlaze.Frontend.Tests.Services;
+
+[TestSubject(typeof(ChatStateService))]
+public class ChatStateServiceTest
+{
+    private ChatStateService _chatStateService;
+
+
+    public ChatStateServiceTest()
+    {
+        //_chatStateService = new ChatStateService();
+    }
+
+    [Fact]
+    public void METHOD()
+    {
+        
+    }
+}

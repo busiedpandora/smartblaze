e1a34b4d4b1629db23cbc6fe56388367699fa61f
simone.giamboni <simone.giamboni@student.supsi.ch>
2024-06-25 17:34:39 +0200

    removed project folder of old backend

--- SmartBlaze/SmartBlaze.Backend_Old/Models/Role.cs
+++
@@ -1,8 +0,0 @@
-namespace SmartBlaze.Backend.Models;
-
-public static class Role
-{
-    public static readonly string User = "user";
-    public static readonly string Assistant = "assistant";
-    public static readonly string System = "system";
-}

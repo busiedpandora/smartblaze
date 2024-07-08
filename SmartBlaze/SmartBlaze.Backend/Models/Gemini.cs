1a41e44a0486344aaf18e318a5b8956de3a22850
simone.giamboni <simone.giamboni@student.supsi.ch>
2024-07-08 16:46:59 +0200

    implemented text streaming for google gemini

--- SmartBlaze/SmartBlaze.Backend/Models/Gemini.cs
+++ SmartBlaze/SmartBlaze.Backend/Models/Gemini.cs
@@ -76,10 +76,73 @@ public class Gemini : Chatbot
         return null;
     }
 
-    public override IAsyncEnumerable<string> GenerateTextStreamEnabled(ChatSessionDto chatSessionDto, List<MessageDto> messageDtos, 
+    public override async IAsyncEnumerable<string> GenerateTextStreamEnabled(ChatSessionDto chatSessionDto, List<MessageDto> messageDtos, 
         HttpClient httpClient)
     {
-        throw new NotImplementedException();
+        var contents = new List<Content>();
+
+        foreach (var message in messageDtos)
+        {
+            var part = new Part
+            {
+                Text = message.Content
+            };
+
+            string? role = message.Role;
+
+            if (message.Role == Role.System)
+            {
+                role = Role.User;
+            }
+            else if (message.Role == Role.Assistant)
+            {
+                role = "model";
+            }
+            
+            var content = new Content
+            {
+                Role = role,
+                Parts = new List<Part>() {part}
+            };
+            contents.Add(content);
+        }
+        
+        var chatRequest = new ChatRequest
+        {
+            Contents = contents
+        };
+        
+        var chatRequestJson = JsonSerializer.Serialize(chatRequest);
+        
+        var httpRequest = new HttpRequestMessage
+        {
+            Method = HttpMethod.Post,
+            RequestUri = new Uri($"{ApiHost}/v1/models/{chatSessionDto.ChatbotModel}:streamGenerateContent?key={ApiKey}"),
+            Content = new StringContent(chatRequestJson, Encoding.UTF8, "application/json")
+        };
+        
+        var chatResponseMessage = await httpClient.SendAsync(httpRequest);
+        chatResponseMessage.EnsureSuccessStatusCode();
+        
+        var chatResponseStream = await chatResponseMessage.Content.ReadAsStreamAsync();
+        var streamReader = new StreamReader(chatResponseStream);
+
+        string? line;
+        
+        while ((line = await streamReader.ReadLineAsync()) != null)
+        {
+            line = line.Trim();
+            if (line.StartsWith("\"text\":"))
+            {
+                line = "{" + line + "}";
+                Part? part = JsonSerializer.Deserialize<Part>(line);
+
+                if (part is not null)
+                {
+                    yield return part.Text ?? "";
+                }
+            }
+        }
     }
 
     private class Part

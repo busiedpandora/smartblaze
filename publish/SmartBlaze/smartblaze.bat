d0f96b22cb77aaeec623a28cb225731031ecb145
simone.giamboni <simone.giamboni@student.supsi.ch>
2024-09-14 00:37:45 +0200

    launch browser

--- publish/SmartBlaze/smartblaze.bat
+++ publish/SmartBlaze/smartblaze.bat
@@ -1,12 +1,14 @@
 @echo off
 
-SET ASPNETCORE_ENVIRONMENT=Development
-
 cd SmartBlaze.Backend
 start SmartBlaze.Backend.exe
 
-timeout /t 5 >nul
+timeout /t 2 >nul
 
 cd ..
 cd SmartBlaze.Frontend
 start SmartBlaze.Frontend.exe
+
+timeout /t 2 >nul
+
+start "" https://localhost:7040
